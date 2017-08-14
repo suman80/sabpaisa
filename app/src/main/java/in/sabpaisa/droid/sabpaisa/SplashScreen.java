77ed9c1c452189a75b1a4097378a92723b72627c
SabPaisa Server <server@srslive.in>
2017-08-14 16:07:58 +0530

    Splash_Screen_USER_Filtering

---
+++ app/src/main/java/in/sabpaisa/droid/sabpaisa/SplashScreen.java
@@ -0,0 +1,69 @@
+package in.sabpaisa.droid.sabpaisa;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class SplashScreen extends AppCompatActivity {
+
+   // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+/*TextView   text =(TextView) findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        });*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, FilterActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
