dc187951e8a4966d7190e2ca36ca982af691aaf9
Sonam Agarwala <sonam.agarwala@srslive.in>
2017-12-15 16:16:42 +0530

    Changes done in UPI_UserAccounts,activity_upi_user_accounts.xml and added bottombar_add_banks

--- app/src/main/java/in/sabpaisa/droid/sabpaisa/UPI_UserAccounts.java
+++ app/src/main/java/in/sabpaisa/droid/sabpaisa/UPI_UserAccounts.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cooltechworks.views.shimmer.ShimmerRecyclerView;
@@ -27,22 +32,73 @@ public class UPI_UserAccounts extends AppCompatActivity implements OliveUpiEvent
     ArrayList<CustomerBankAccounts> bankAccounts;
     ArrayList<Account> userBankAccountDetailsArrayList;
     Account account;
+    Toolbar mtoolbar;
+    TextView accountName,accountNumber,accountStatus,accountType,accountVPA,requestBalance;
+    FrameLayout addNewBankAccount;
+    Button accountRemove,setUpiPin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         CommonUtils.setFullScreen(this);
         setContentView(R.layout.activity_upi__user_accounts);
+
+
         OliveUpiManager.getInstance(UPI_UserAccounts.this).setListener(this);
 
         OliveUpiManager.getInstance(UPI_UserAccounts.this).fetchMyAccounts();
-        userBankAccountsRecyclerView = (ShimmerRecyclerView)findViewById(R.id.userBankAccountsRecyclerView);
+        mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*userBankAccountsRecyclerView = (ShimmerRecyclerView)findViewById(R.id.userBankAccountsRecyclerView);
         LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
-        userBankAccountsRecyclerView.setLayoutManager(llm);
+        userBankAccountsRecyclerView.setLayoutManager(llm);*/
 
+        accountName = (TextView)findViewById(R.id.accountName);
+        accountNumber = (TextView)findViewById(R.id.accountNumber);
+        //accountStatus = (TextView)itemView.findViewById(R.id.accountStatus);
+        accountType = (TextView)findViewById(R.id.accountType);
+        accountVPA = (TextView)findViewById(R.id.accountVPA);
+        requestBalance = (TextView)findViewById(R.id.requestBalance);
+        addNewBankAccount=(FrameLayout)findViewById(R.id.addNewBankAccount);
+        accountRemove = (Button)findViewById(R.id.accountRemove);
+        setUpiPin = (Button)findViewById(R.id.setUpiPin);
+        mtoolbar.setTitle("User Account Details");
+        setSupportActionBar(mtoolbar);
 
+        requestBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (account == null) {
+                    Toast.makeText(UPI_UserAccounts.this, "No account selected.", Toast.LENGTH_SHORT).show();
+                } else {
+                    OliveUpiManager.getInstance(UPI_UserAccounts.this).checkBalance(account);
+                }
+            }
+        });
 
+        addNewBankAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UPI_UserAccounts.this,UPIBankList.class);
+                startActivity(intent);
+
+            }
+        });
+
+        accountRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OliveUpiManager.getInstance(UPI_UserAccounts.this). accountRemove(account);
+            }
+        });
+
+        setUpiPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String bankid=account.getIin().toString(); //here bankid is IIN number
+                OliveUpiManager.getInstance(UPI_UserAccounts.this).changeMpin(bankid,account);
+            }
+        });
 
     }
 
@@ -63,22 +119,61 @@ public class UPI_UserAccounts extends AppCompatActivity implements OliveUpiEvent
 
                 userBankAccountDetailsArrayList = new ArrayList<>();
                 userBankAccountDetailsArrayList=bankAccounts.get(0).getAccounts();
-                UPI_UserAccountCustomAdapter upi_userAccountCustomAdapter = new UPI_UserAccountCustomAdapter(userBankAccountDetailsArrayList);
-                userBankAccountsRecyclerView.setAdapter(upi_userAccountCustomAdapter);
+                /*UPI_UserAccountCustomAdapter upi_userAccountCustomAdapter = new UPI_UserAccountCustomAdapter(userBankAccountDetailsArrayList);
+                userBankAccountsRecyclerView.setAdapter(upi_userAccountCustomAdapter);*/
 
                 account=(Account)bankAccounts.get(0).getAccounts().get(0);
-                //tv_accountNumber.setText(account.getMaskedAccnumber());
+
+                accountName.setText(userBankAccountDetailsArrayList.get(0).getName());
+                accountNumber.setText(userBankAccountDetailsArrayList.get(0).getMaskedAccnumber());
+                accountType.setText(userBankAccountDetailsArrayList.get(0).getType());
+                accountVPA.setText(userBankAccountDetailsArrayList.get(0).getVpa());
+
                 Log.d("Val_Acc","-->"+account.toString());
 
 
             }
-
-            //This API is used to verify the virtual Payment Address is valid or not.
         }
 
-        else{
+
+        //This API is used to check balance in the account based on Account details/VPA /Customer ID.(7)
+        else if (reqType== UpiService. REQUEST_GET_BALANCE){
+
+            Result<String> checkBalance = (Result<String>) data;
+            Log.d("REQUEST_GET_BALANCE","--->"+checkBalance);
+            requestBalance.setText(checkBalance.getData());
+
+        }
+
+        //This API is used to change the existing Mpin and add the new Mpin
+        else if (reqType== UpiService. REQUEST_GET_CHANGE_MPIN){
+            Result<ArrayList<Account>> changeMPin = (Result<ArrayList<Account>>) data;
+            Log.d("REQUEST_GET_CHANGE_MPIN","-->"+changeMPin);
+            Toast.makeText(this,"MPin Change Success",Toast.LENGTH_SHORT).show();
+        }
+
+
+        //This API is used to remove the particular account from the list of added accounts.
+        else if (reqType== UpiService. REQUEST_GET_ACCOUNT_REMOVE){
+            Result<String> accountRemove = (Result<String>) data;
+
+            if (accountRemove.getCode().equals("00")){
+                Log.d("accountRemove","--->"+accountRemove);
+                Toast.makeText(this,"Account Removed Successfully",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+        else {
             Toast.makeText(this,"Not Success",Toast.LENGTH_SHORT).show();
         }
+
+
+
+
+
+
     }
 
     @Override
